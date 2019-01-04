843eac4fecd90b0d96e08603b1fabc96b5ae78e4
LaupWing <loc_strict@hotmail.com>
2019-01-04 14:42:46 +0100

    id kaart gemaakt

--- client/src/components/gebruikersInput/gebruikersInputComponents/Veiligheid.vue
+++ client/src/components/gebruikersInput/gebruikersInputComponents/Veiligheid.vue
@@ -1,12 +1,28 @@
 <template>
     <div class="veiligheid flexCenter">
         <div class="tekst">
-            <h2>Hoe veilig voel jij je rondom poltie-agenten?</h2>
+            <h2>Hoe onveilig voel jij je rondom poltie-agenten?</h2>
         </div>
         <div class="paspoort">
             <div class="paspoort-logo flexCenter">Paspoort</div>
+            <p class="cad">Control Alt Delete</p>
             <!-- <img src="../../../assets/Avatar/man/jong.png" alt=""> -->
-            <img v-bind:src="imgUrl()" alt="">
+            <div class="img-container">
+                <img v-bind:src="imgUrl()" alt="">
+                <p class="veiligheid">Onveiligheidsgevoel: {{veiligheidsGevoel}}</p>
+            </div>
+            <div class="info">
+                <p class="label">Afkomst</p>
+                {{afkomst}}
+                <p class="label">Woonplaats</p>
+                {{woonplaats}}
+                <p class="label">Gemaakt op</p>
+                {{gemaakt.getDate()}}/{{gemaakt.getMonth()}}/{{gemaakt.getFullYear()}}
+                <p class="label">Leeftijd</p>
+                {{leeftijd}}
+                <p class="label">Geslacht</p>
+                {{geslacht}}
+            </div>
         </div>
     </div>
 </template>
@@ -21,7 +37,8 @@ export default {
             woonplaats: this.userInfo.woonplaats,
             geslacht: this.userInfo.geslacht,
             afkomst: this.userInfo.afkomst,
-            veiligheidsGevoel: 0
+            veiligheidsGevoel: 0,
+            gemaakt: new Date()
         }
     },
     methods:{
@@ -30,7 +47,7 @@ export default {
         },
     },
     beforeMount(){
-        console.log(this.avatarPath)
+        console.log(this.gemaakt)
     }
 }
 </script>
@@ -44,23 +61,50 @@ export default {
     box-shadow: 15px 14px 5px 1px rgba(0,0,0,0.3);
 }
 .paspoort-logo{
+    width: 20%;
     background: blue;
-    position: absolute;
     top: 10px;
     left: 10px;
     color: white;
     padding: 10px 15px;
     text-transform: uppercase;
-    letter-spacing: 2px;
+    letter-spacing: 5px;
+}
+.paspoort > * {
+    position: absolute;
 }
 .tekst{
     margin-bottom: 50px;
 }
 img{
     width: 180px;
-    position: absolute;
+    height: 250px;
+}
+.img-container{
     left: 20px;
-    top: 70px;
+    top: 60px;
+    padding: 5px;
+}
+.cad{
+    top: 17px;
+    text-transform: uppercase;
+    color: blue;
+    left: 230px;
+    letter-spacing: 8px;
+    font-size: 20px;
+}
+.info{
+    left: 220px;
+    top: 60px;
+    text-align: left;
+}
+.label{
+    background: lightblue;
+    width: 300px;
+    /* margin: 5px; */
+    margin-top: 10px;
+    padding: 3px;
+    font-size: 12px;
 }
 </style>
 
