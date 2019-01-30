102a91b318d54948843a19537af9ede41ad9b164
LaupWing <loc_strict@hotmail.com>
2019-01-30 16:25:29 +0100

    Update Recent-resultaat.vue

---
+++ client/src/components/gebruikersInput/InputPaginas/ResultatenComponents/Recent-resultaat.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div class="artikel">
+
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+.artikel{
+    width: 100%;
+    height: 100px;
+    background: rgba(255,255,255,.8);
+}
+</style>
